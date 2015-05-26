fd9655e5a57677db67153ead250cfc170d16b6f5
Victor Gasparotto Capone <ra177952@xaveco.lab.ic.unicamp.br>
2015-05-26 11:50:17 -0300

    .

--- test.sh
+++ test.sh
@@ -34,43 +34,51 @@ fi
 
 
 # Compila
-$(gcc $code.c -o $code -lm)
+echo -e "\033[31m I \033[0m Compilando $code.c"
+$(gcc $code.c -o $code -lm >> $dir/compilation)
 
 # Para cada arquivo em $dir com final .in
+errors=$(wc -c $dir/compilation | cut -d" " -f1)
+if [ $errors -ge 1 ]; then
+	echo "\033[31m W \033[0m Houveram erros na compilação"
+	echo "\033[33m+--------------------------------------------------------+"
+	cat $dir/compilation
+	rm -f $dir/compilation
+else
 
-echo -e "\033[31m I \033[0m Rodando testes"
+	echo -e "\033[31m I \033[0m Rodando testes"
 
-for filename in $(pwd)/$dir/*.in; do
+	for filename in $(pwd)/$dir/*.in; do
 
-	# Retira a extensao do nome do arquivo
-	file="${filename%.*}"
+		# Retira a extensao do nome do arquivo
+		file="${filename%.*}"
 
-	# Itera pelos testes
-	printf "\033[33m R \033[0m Teste \033[1m%02d\033[0m... " $i
+		# Itera pelos testes
+		printf "\033[33m R \033[0m Teste \033[1m%02d\033[0m... " $i
 
-	# Cria a saida de acordo com a entrada
-	$(./$code < $file.in > $file.out)
+		# Cria a saida de acordo com a entrada
+		$(./$code < $file.in > $file.out)
 
-	# Compara com o resultado esperado e
-	# diz se o programa passou ou nao
+		# Compara com o resultado esperado e
+		# diz se o programa passou ou nao
 
-	$(diff $file.out $file.res > $dir/thediff 2>&1)
+		$(diff $file.out $file.res > $dir/thediff 2>&1)
 
-	if [ $? != 0 ] 
-	then
-		echo -e "\033[31m FAIL \033[0m"
-		echo -e " \033[1m+--------------------------------------------------------+"
-		echo -e "\033[32m EXPECTED\n \033[0m $(cat $dir/thediff | grep '>')"
-		echo -e "\033[31m ACTUAL\n \033[0m $(cat $dir/thediff | grep '<')"
-		echo -e " \033[1m+--------------------------------------------------------+"
-	else
-		echo -e "\033[32m OK"
-	fi
-
-	# Remove o arquivo de saida
-	$(rm $file.out)
-	$(rm $dir/thediff)
-
-	# i += 1
-	i=`expr $i + 1`
-done
+		if [ $? != 0 ] 
+		then
+			echo -e "\033[31m FAIL \033[0m"
+			echo -e " \033[1m+--------------------------------------------------------+"
+			echo -e "\033[32m EXPECTED\n \033[0m $(cat $dir/thediff | grep '>')"
+			echo -e "\033[31m ACTUAL\n \033[0m $(cat $dir/thediff | grep '<')"
+			echo -e " \033[1m+--------------------------------------------------------+"
+		else
+			echo -e "\033[32m OK"
+		fi
+		# Remove os arquivo de saida
+		$(rm $file.out)
+		$(rm $dir/thediff)
+		# i += 1
+		i=`expr $i + 1`
+	done
+fi
+rm -f -r $dir
